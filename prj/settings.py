4bc39954f8d4507045a85c8c132e10ad4eca9269
a8vk <a8vk@ya.ru>
2023-04-23 08:18:27 +1000

    Group of users

--- prj/settings.py
+++ prj/settings.py
@@ -61,6 +61,7 @@ ACCOUNT_UNIQUE_EMAIL = True
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_FORMS = {"signup": "accounts.forms.CustomSignupForm"}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
