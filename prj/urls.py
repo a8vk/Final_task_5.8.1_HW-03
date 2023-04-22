0957572ec5b0e74c37c038c696ad17fee0e0a4e9
a8vk <a8vk@ya.ru>
2023-04-22 20:02:49 +1000

    registration of new users

--- prj/urls.py
+++ prj/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path("accounts/", include("accounts.urls")),  # Добавили эту строчку
     path('news/', include('news.urls'))
 ]
