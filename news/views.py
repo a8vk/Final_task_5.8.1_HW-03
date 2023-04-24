b382111497949caedb1fa1c93c627b089e744e05
a8vk <a8vk@ya.ru>
2023-04-24 22:30:14 +1000

    Registration menu

--- news/views.py
+++ news/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.urls import reverse_lazy
 from .models import New
 from .filters import NewFilter
@@ -73,38 +73,43 @@ def news_search(request):
     return render(request, 'news_search.html', {'filter': news_filter, 'news_list': news_filter.qs})
 
 
-class UncosCreateView(LoginRequiredMixin, CreateView):
-    raise_exception = True
+class UncosCreateView(PermissionRequiredMixin, CreateView):
+    permission_required = ('news.add_new',)
     model = New
     fields = ['title', 'text', 'category']
     template_name = 'news_form.html'
 
 
-class UncosUpdateView(UpdateView):
+class UncosUpdateView(PermissionRequiredMixin, UpdateView):
+    permission_required = ('news.change_new',)
     model = New
     fields = ['title', 'text', 'category']
     template_name = 'news_edit.html'
 
 
-class UncosDeleteView(DeleteView):
+class UncosDeleteView(PermissionRequiredMixin, DeleteView):
+    permission_required = ('news.delete_new',)
     model = New
     success_url = reverse_lazy('index')
     template_name = 'news_confirm_delete.html'
 
 
-class ArticlesCreateView(CreateView):
+class ArticlesCreateView(PermissionRequiredMixin, CreateView):
+    permission_required = ('news.add_new',)
     model = New
     fields = ['title', 'text', 'category']
     template_name = 'articles_form.html'
 
 
-class ArticlesUpdateView(UpdateView):
+class ArticlesUpdateView(PermissionRequiredMixin, UpdateView):
+    permission_required = ('news.change_new',)
     model = New
     fields = ['title', 'text', 'category']
     template_name = 'articles_edit.html'
 
 
-class ArticlesDeleteView(DeleteView):
+class ArticlesDeleteView(PermissionRequiredMixin, DeleteView):
+    permission_required = ('news.delete_new',)
     model = New
     success_url = reverse_lazy('articles')
     template_name = 'articles_confirm_delete.html'
